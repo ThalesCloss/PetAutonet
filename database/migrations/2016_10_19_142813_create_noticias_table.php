d9f2d7c855cc630bbb45c3b130702202bdcac6fb
Thales Closs <thales.a.closs@gmail.com>
2016-11-07 18:46:35 -0300

    Validação CPF

--- database/migrations/2016_10_19_142813_create_noticias_table.php
+++ database/migrations/2016_10_19_142813_create_noticias_table.php
@@ -19,7 +19,8 @@ class CreateNoticiasTable extends Migration
             $table->longText('texto');
             #$table->json('palavras_chave');
             $table->timestamp('publicacao');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();;
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2016_11_05_223000_create_papel_usuarios_table.php
+++ database/migrations/2016_11_05_223000_create_papel_usuarios_table.php
@@ -15,7 +15,7 @@ class CreatePapelUsuariosTable extends Migration
     {
         Schema::create('papel_usuarios', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();;
+            $table->integer('user_id')->unsigned();
             $table->enum('papel',['ADMIN','NOTICIAS','CURSOS','PETEANOS']);
             $table->foreign('user_id')->references('id')->on('users');
         });
